50ed75a1c259246f620e1117ceefef019c45f865
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-12-03 01:32:10 -0500

    main: Adding basic testing code.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- main.c
+++ main.c
@@ -1,18 +1,48 @@
 #include "batch.h"
+#include "dispatcher.h"
+#include "job_dummy.h"
 #include "log.h"
 
-#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define POOL_SIZE 5
 
 #define LOG_TYPE LT_MAIN
 
 int main(void) {
+	struct batch* b;
+	struct dispatcher* d;
+	int result;
 
 	log_init(stderr);
-	
+
+	LOG(LL_DEBUG, "initializing objects");
+	d = dispatcher_create("dtest");
+	b = batch_create("btest");
+
+	if ((result = batch_add_job(b, job_dummy_create()))) {
+		LOG(LL_ERROR, "batch_add_job: %s", strerror(result));
+		dispatcher_destroy(d);
+		batch_decref(b);	
+		return EXIT_FAILURE;
+	}
+
+	if ((result = dispatcher_add_batch(d, b))) {
+		LOG(LL_ERROR, "dispatcher_add_batch: %s", strerror(result));
+		dispatcher_destroy(d);
+		batch_decref(b);
+		return EXIT_FAILURE;
+	}
+
+	LOG(LL_DEBUG, "done initializing objects");
+
+	LOG(LL_DEBUG, "starting dispatcher");
+	if ((result = dispatcher_run(d))) {
+		LOG(LL_ERROR, "dispatcher_run: %s", strerror(result));
+	} else {
+		LOG(LL_DEBUG, "dispatcher exited successfully");
+	}
+
+	dispatcher_destroy(d);
 
 	return EXIT_SUCCESS;
 }
