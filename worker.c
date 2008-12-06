1113bc9d41396278e1454fb94af104732b80d8d7
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-12-06 01:03:03 -0500

    worker: Adding a basic thread model to workers.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- worker.c
+++ worker.c
@@ -12,20 +12,49 @@
 struct job;
 
 struct worker {
-	struct job*     w_job;
-	pthread_cond_t  w_cv;
-	pthread_mutex_t w_mutex;
+	struct job*       w_job;
+	pthread_cond_t    w_cv;
+	pthread_mutex_t   w_mutex;
+	pthread_t         w_thread;
+	enum worker_state w_state;
+	int               w_shutdown;
 };
 
+/******************************************************************************/
+int worker_impl(struct worker* w) {
+	int result;
+
+	LOG(LL_DEBUG, "worker started");
+
+	CHECK_LOCK(w->w_mutex);
+	while (w->w_shutdown == 0) {
+		pthread_cond_wait(&w->w_cv, &w->w_mutex);
+	}
+	CHECK_UNLOCK(w->w_mutex);
+
+	return 0;
+}
+
+/******************************************************************************/
+static void* worker_thread(void* arg) {
+	worker_impl(arg);
+	return NULL;
+}
+
+/******************************************************************************/
 struct worker* worker_create() {
 	struct worker* w = NULL;
 	int result;
 
 	w = malloc(sizeof(struct worker));
 	w->w_job = NULL;
+	w->w_state = WS_WAITING;
+	w->w_shutdown = 0;
 
 	CHECK_LOCK_INIT(w->w_mutex);
 	CHECK_COND_INIT(w->w_cv);
+
+	pthread_create(&w->w_thread, NULL, worker_thread, w);
 
 	return w;
 failure:
@@ -33,6 +62,7 @@ failure:
 	return NULL;
 }
 
+/******************************************************************************/
 void worker_destroy(struct worker* w) {
 	if (!w) {
 		LOG(LL_WARN, "null worker");
@@ -47,4 +77,23 @@ void worker_destroy(struct worker* w) {
 	pthread_cond_destroy(&w->w_cv);
 
 	free(w);
+}
+
+/*****************************************************************************/
+enum worker_state worker_state(struct worker* w) {
+	return w->w_state;
+}
+
+/*****************************************************************************/
+int worker_shutdown(struct worker* w) {
+	int result;
+
+	PRE(w != NULL);
+
+	CHECK_LOCK(w->w_mutex);
+	w->w_shutdown = 1;
+	pthread_cond_broadcast(&w->w_cv);
+	CHECK_UNLOCK(w->w_mutex);
+
+	return 0;
 }
--- worker.h
+++ worker.h
@@ -3,7 +3,14 @@
 
 struct worker;
 
-struct worker* worker_create();
-void           worker_destroy(struct worker*);
+enum worker_state {
+	WS_WAITING,
+	WS_PROCESSING,
+};
+
+struct worker*    worker_create();
+void              worker_destroy(struct worker*);
+enum worker_state worker_state(struct worker*);
+int               worker_shutdown(struct worker*);
 
 #endif
