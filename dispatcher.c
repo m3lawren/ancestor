30c4f7a80cd4476b27a5b0cd59ff5766b538237c
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-12-05 23:34:15 -0500

    dispatcher: Adding mutexes/cvs.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- dispatcher.c
+++ dispatcher.c
@@ -6,19 +6,23 @@
 
 #include <array.h>
 #include <errno.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define LOG_TYPE LT_DISPATCHER
 
 struct dispatcher {
-	char*         d_name;
-	struct array* d_batches;
-	unsigned int  d_num_workers;
+	char*           d_name;
+	struct array*   d_batches;
+	unsigned int    d_num_workers;
+	pthread_cond_t  d_cv;
+	pthread_mutex_t d_mutex;
 };
 
 /*****************************************************************************/
 struct dispatcher* dispatcher_create(const char* name) {
+	int result;
 	struct dispatcher* d;
 
 	if (!(d = calloc(1, sizeof(struct dispatcher)))) {
@@ -35,6 +39,16 @@ struct dispatcher* dispatcher_create(const char* name) {
 
 	if (!(d->d_batches = array_create())) {
 		LOG(LL_ERROR, "unable to allocate space for batches");
+		goto failure;
+	}
+
+	if ((result = pthread_mutex_init(&d->d_mutex, NULL))) {
+		LOG(LL_ERROR, "pthread_mutex_init: %s", strerror(result));
+		goto failure;
+	}
+
+	if ((result = pthread_cond_init(&d->d_cv, NULL))) {
+		LOG(LL_ERROR, "pthread_cond_init: %s", strerror(result));
 		goto failure;
 	}
 
@@ -67,6 +81,10 @@ int dispatcher_destroy(struct dispatcher* d) {
 		}
 		array_destroy(d->d_batches);
 	}
+
+	pthread_cond_destroy(&d->d_cv);
+	pthread_mutex_destroy(&d->d_mutex);
+
 	free(d);
 
 	return 0;
@@ -81,20 +99,30 @@ int dispatcher_run(struct dispatcher* d) {
 
 /*****************************************************************************/
 int dispatcher_add_batch(struct dispatcher* d, struct batch* b) {
+	int result;
+
 	CHECK_NULL(d);
 	CHECK_NULL(b);
 
 	LOG(LL_DEBUG, "adding batch %s to dispatcher %s", batch_name(b), d->d_name);
 
-	return array_append(d->d_batches, b);
+	CHECK_LOCK(d->d_mutex);
+	result = array_append(d->d_batches, b);
+	CHECK_UNLOCK(d->d_mutex);
+
+	return result;
 }
 
 /*****************************************************************************/
 int dispatcher_set_workers(struct dispatcher* d, unsigned int n) {
+	int result;
+
 	CHECK_NULL(d);
 	CHECK_COND(n > 0 && n <= DISPATCHER_WORKERS_MAX);
-	
+
+	CHECK_LOCK(d->d_mutex);
 	d->d_num_workers = n;
+	CHECK_UNLOCK(d->d_mutex);
 
 	return 0;
 }
