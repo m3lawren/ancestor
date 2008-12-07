af6f9fdae22dd6da7abd7fda939c92cd21f8a469
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-12-07 16:55:24 -0500

    worker: Adding an error state, making it call dispatcher_notify, and also adding worker_job.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- worker.c
+++ worker.c
@@ -1,6 +1,8 @@
 #include "worker.h"
 
 #include "check.h"
+#include "dispatcher.h"
+#include "job.h"
 #include "log.h"
 
 #include <pthread.h>
@@ -12,23 +14,50 @@
 struct job;
 
 struct worker {
-	struct job*       w_job;
-	pthread_cond_t    w_cv;
-	pthread_mutex_t   w_mutex;
-	pthread_t         w_thread;
-	enum worker_state w_state;
-	int               w_shutdown;
+	struct job*        w_job;
+	struct dispatcher* w_dispatcher;
+	pthread_cond_t     w_cv;
+	pthread_mutex_t    w_mutex;
+	pthread_t          w_thread;
+	enum worker_state  w_state;
+	int                w_shutdown;
 };
 
 /******************************************************************************/
 int worker_impl(struct worker* w) {
 	int result;
 
+	PRE(w->w_thread == pthread_self());
+
 	LOG(LL_DEBUG, "worker started");
 
 	CHECK_LOCK(w->w_mutex);
-	while (w->w_shutdown == 0) {
-		pthread_cond_wait(&w->w_cv, &w->w_mutex);
+	while (1) {
+		while (w->w_shutdown == 0 && 
+				(w->w_job == NULL || 
+				 w->w_job->j_state == JS_COMPLETE ||
+				 w->w_job->j_state == JS_ERROR)) {
+			pthread_cond_wait(&w->w_cv, &w->w_mutex);
+		}
+
+		if (w->w_shutdown) {
+			break;
+		}
+
+		CHECK_CALL(job_run(w->w_job)) { 
+			LOG_CALL_WARN(job_run);
+		}
+
+		if (w->w_job->j_state != JS_COMPLETE) {
+			w->w_job->j_state = JS_ERROR;
+		}
+
+		CHECK_CALL(dispatcher_notify(w->w_dispatcher, w)) {
+			LOG_CALL_ERROR(dispatcher_notify);
+			w->w_state = WS_ERROR;
+			CHECK_UNLOCK(w->w_mutex);
+			return result;
+		}
 	}
 	w->w_state = WS_SHUTDOWN;
 	CHECK_UNLOCK(w->w_mutex);
@@ -43,7 +72,7 @@ static void* worker_thread(void* arg) {
 }
 
 /******************************************************************************/
-struct worker* worker_create() {
+struct worker* worker_create(struct dispatcher* d) {
 	struct worker* w = NULL;
 	int result;
 
@@ -51,6 +80,7 @@ struct worker* worker_create() {
 	w->w_job = NULL;
 	w->w_state = WS_WAITING;
 	w->w_shutdown = 0;
+	w->w_dispatcher = d;
 
 	CHECK_LOCK_INIT(w->w_mutex);
 	CHECK_COND_INIT(w->w_cv);
@@ -81,11 +111,6 @@ void worker_destroy(struct worker* w) {
 }
 
 /*****************************************************************************/
-enum worker_state worker_state(struct worker* w) {
-	return w->w_state;
-}
-
-/*****************************************************************************/
 int worker_shutdown(struct worker* w) {
 	int result;
 
@@ -97,4 +122,15 @@ int worker_shutdown(struct worker* w) {
 	CHECK_UNLOCK(w->w_mutex);
 
 	return 0;
+}
+
+
+/*****************************************************************************/
+enum worker_state worker_state(struct worker* w) {
+	return w->w_state;
+}
+
+/*****************************************************************************/
+struct job* worker_job(struct worker* w) {
+	return w->w_job;
 }
--- worker.h
+++ worker.h
@@ -3,15 +3,20 @@
 
 struct worker;
 
+struct dispatcher;
+
 enum worker_state {
 	WS_WAITING,
 	WS_PROCESSING,
 	WS_SHUTDOWN,
+	WS_ERROR,
 };
 
-struct worker*    worker_create();
+struct worker*    worker_create(struct dispatcher*);
 void              worker_destroy(struct worker*);
-enum worker_state worker_state(struct worker*);
 int               worker_shutdown(struct worker*);
+
+enum worker_state worker_state(struct worker*);
+struct job*       worker_job(struct worker*);
 
 #endif
